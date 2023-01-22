9f5c5c27755bc7c8d977e248dc43461d116e2fcd
Shubham <shubhamkashyap0608@gmail.com>
2023-01-22 05:46:31 +0530

    Login and Register page linked with Firebase and are functional.

--- lib/register.dart
+++ lib/register.dart
@@ -1,5 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
+
+import 'DashBoard.dart';
+import 'model/user_model.dart';
 
 
 
@@ -14,6 +20,29 @@ class MyRegister extends StatefulWidget {
 }
 
 class _MyRegisterState extends State<MyRegister> {
+
+// Firebase auth
+  final _auth = FirebaseAuth.instance;
+
+  // string for displaying the error Message
+  String? errorMessage;
+
+
+  // our form key
+  final _formKey = GlobalKey<FormState>();
+  // editing Controller
+  final nameEditingController = new TextEditingController();
+  final emailEditingController = new TextEditingController();
+  final passwordEditingController = new TextEditingController();
+  final confirmPasswordEditingController = new TextEditingController();
+  final phoneNumberEditingController = new TextEditingController();
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -39,7 +68,7 @@ class _MyRegisterState extends State<MyRegister> {
             SingleChildScrollView(
               child: Container(
                 padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.28),
+                    top: MediaQuery.of(context).size.height * 0.253),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -47,9 +76,30 @@ class _MyRegisterState extends State<MyRegister> {
                       margin: EdgeInsets.only(left: 35, right: 35),
                       child: Column(
                         children: [
-                          TextField(
+                          TextFormField(
                             style: TextStyle(color: Colors.white),
+
+                            autofocus: false,
+                            controller: nameEditingController,
+                            keyboardType: TextInputType.name,
+                            validator: (value) {
+                              RegExp regex = new RegExp(r'^.{3,}$');
+                              if (value!.isEmpty) {
+                                return ("Name cannot be Empty");
+                              }
+                              if (!regex.hasMatch(value)) {
+                                return ("Enter Valid name(Min. 3 Character)");
+                              }
+                              return null;
+                            },
+
+                            onSaved: (value) {
+                              nameEditingController.text = value!;
+                            },
+                            textInputAction: TextInputAction.next,
+
                             decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.account_circle),
                                 enabledBorder: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(10),
                                   borderSide: BorderSide(
@@ -69,11 +119,33 @@ class _MyRegisterState extends State<MyRegister> {
                                 )),
                           ),
                           SizedBox(
-                            height: 30,
+                            height: 16,
                           ),
-                          TextField(
+                          TextFormField(
+
+                            autofocus: false,
+                            controller: emailEditingController,
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return ("Please Enter Your Email");
+                              }
+                              // reg expression for email validation
+                              if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                                  .hasMatch(value)) {
+                                return ("Please Enter a valid email");
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              emailEditingController.text = value!;
+                            },
+                            textInputAction: TextInputAction.next,
+
+
                             style: TextStyle(color: Colors.white),
                             decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.mail),
                                 enabledBorder: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(10),
                                   borderSide: BorderSide(
@@ -93,12 +165,36 @@ class _MyRegisterState extends State<MyRegister> {
                                 )),
                           ),
                           SizedBox(
-                            height: 30,
+                            height: 16,
                           ),
-                          TextField(
-                            style: TextStyle(color: Colors.white),
+
+
+                          TextFormField(
+
+                            autofocus: false,
+                            controller: passwordEditingController,
                             obscureText: true,
+                            validator: (value) {
+                              RegExp regex = new RegExp(r'^.{6,}$');
+                              if (value!.isEmpty) {
+                                return ("Password is required for login");
+                              }
+                              if (!regex.hasMatch(value)) {
+                                return ("Enter Valid Password(Min. 6 Character)");
+                              }
+                            },
+                            onSaved: (value) {
+                              passwordEditingController.text = value!;
+                            },
+                            textInputAction: TextInputAction.next,
+
+
+                            style: TextStyle(color: Colors.white),
+
+
+                            //obscureText: true,
                             decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.vpn_key),
                                 enabledBorder: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(10),
                                   borderSide: BorderSide(
@@ -118,7 +214,52 @@ class _MyRegisterState extends State<MyRegister> {
                                 )),
                           ),
                           SizedBox(
-                            height: 40,
+                            height: 16,
+                          ),
+                          TextFormField(
+
+                            autofocus: false,
+                            controller: confirmPasswordEditingController,
+                            obscureText: true,
+                            validator: (value) {
+                              if (confirmPasswordEditingController.text !=
+                                  passwordEditingController.text) {
+                                return "Password don't match";
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              confirmPasswordEditingController.text = value!;
+                            },
+                            textInputAction: TextInputAction.next,
+
+
+                            style: TextStyle(color: Colors.white),
+
+
+                            //obscureText: true,
+                            decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.vpn_key),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                hintText: "Confirm Password",
+                                hintStyle: TextStyle(color: Colors.white),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          SizedBox(
+                            height: 16,
                           ),
 
 
@@ -169,12 +310,15 @@ class _MyRegisterState extends State<MyRegister> {
                                     fontSize: 27,
                                     fontWeight: FontWeight.w700),
                               ),
+
                               CircleAvatar(
                                 radius: 30,
-                                backgroundColor: Color(0xff4c505b),
+                                backgroundColor: Colors.blue,
                                 child: IconButton(
                                     color: Colors.white,
-                                    onPressed: () {},
+                                    onPressed: () {
+                                      signUp(emailEditingController.text, passwordEditingController.text);
+                                    },
                                     icon: Icon(
                                       Icons.arrow_forward,
                                     )),
@@ -182,7 +326,7 @@ class _MyRegisterState extends State<MyRegister> {
                             ],
                           ),
                           SizedBox(
-                            height: 40,
+                            height: 10,
                           ),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -215,4 +359,72 @@ class _MyRegisterState extends State<MyRegister> {
       ),
     );
   }
+
+
+
+  void signUp(String email, String password) async {
+    //if (_formKey.currentState!.validate()) {
+      try {
+        await _auth
+            .createUserWithEmailAndPassword(email: email, password: password)
+            .then((value) => {postDetailsToFirestore()})
+            .catchError((e) {
+          Fluttertoast.showToast(msg: e!.message);
+        });
+      } on FirebaseAuthException catch (error) {
+        switch (error.code) {
+          case "invalid-email":
+            errorMessage = "Your email address appears to be malformed.";
+            break;
+          case "wrong-password":
+            errorMessage = "Your password is wrong.";
+            break;
+          case "user-not-found":
+            errorMessage = "User with this email doesn't exist.";
+            break;
+          case "user-disabled":
+            errorMessage = "User with this email has been disabled.";
+            break;
+          case "too-many-requests":
+            errorMessage = "Too many requests";
+            break;
+          case "operation-not-allowed":
+            errorMessage = "Signing in with Email and Password is not enabled.";
+            break;
+          default:
+            errorMessage = "An undefined Error happened.";
+        }
+        Fluttertoast.showToast(msg: errorMessage!);
+        print(error.code);
+      }
+    //}
+  }
+  postDetailsToFirestore() async {
+    // calling our firestore
+    // calling our user model
+    // sedning these values
+
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    User? user = _auth.currentUser;
+
+    UserModel userModel = UserModel();
+
+    // writing all the values
+    userModel.email = user!.email;
+    userModel.uid = user.uid;
+    userModel.name = nameEditingController.text;
+    userModel.phoneNumber = phoneNumberEditingController.text;
+
+    await firebaseFirestore
+        .collection("users")
+        .doc(user.uid)
+        .set(userModel.toMap());
+    Fluttertoast.showToast(msg: "Account created successfully :) ");
+
+    Navigator.pushAndRemoveUntil(
+        (context),
+        MaterialPageRoute(builder: (context) => BottomNavBar()),
+            (route) => false);
+  }
+
 }
