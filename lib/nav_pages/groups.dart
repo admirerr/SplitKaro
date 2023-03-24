92438f7456654e849f37b45f5d97d689daa3a534
Shubham <shubhamkashyap0608@gmail.com>
2023-03-24 09:33:05 +0530

    Added Create Group pop up

--- lib/nav_pages/groups.dart
+++ lib/nav_pages/groups.dart
@@ -8,17 +8,11 @@ import '../model/user_model.dart';
 class groupsPage extends StatefulWidget {
   const groupsPage({Key? key}) : super(key: key);
 
-
   @override
   State<groupsPage> createState() => _groupsPageState();
 }
 
 class _groupsPageState extends State<groupsPage> {
-
-
-
-
-
   User? user = FirebaseAuth.instance.currentUser;
   UserModel loggedInUser = UserModel();
 
@@ -35,47 +29,103 @@ class _groupsPageState extends State<groupsPage> {
     });
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    var arrNames = ['Group','Group','Group','Group','Group','Group','Group','Group'];
+    var arrNames = [
+      'Group',
+      'Group',
+      'Group',
+      'Group',
+      'Group',
+      'Group',
+      'Group',
+      'Group'
+    ];
     return Scaffold(
       body: Container(
-
         color: Colors.blue.withOpacity(.4),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               AppBar(
-                title: Text("Groups", textScaleFactor: 1.5, style: TextStyle(fontFamily: 'Dosis' ,color: Colors.black ,fontWeight: FontWeight.w500),),
-                backgroundColor: Colors.white70, centerTitle: true,
-                actions: <Widget>
-                [
-                  new IconButton(
-                    onPressed:()
-                    {
-
+                title: Text(
+                  "Groups",
+                  textScaleFactor: 1.5,
+                  style: TextStyle(
+                      fontFamily: 'Dosis',
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500),
+                ),
+                backgroundColor: Colors.white70,
+                centerTitle: true,
+                actions: <Widget>[
+                  IconButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) =>
+                            AlertDialog(
+                              title: Text('New Group'),
+                              content: TextField(
+                                decoration: InputDecoration(
+                                  hintText: 'Enter group name',
+                                ),
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text('Cancel'),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    // TODO: add new group to Firestore
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text('Create'),
+                                ),
+                              ],
+                            ),
+                      );
                     },
-                      icon: Icon(Icons.add_circle_sharp, size: 35, color: Colors.blueAccent,),
+                    icon: Icon(
+                      Icons.add_circle_sharp,
+                      size: 35,
+                      color: Colors.blueAccent,
+                    ),
                   ),
                 ],
-                ),
+              ),
               Expanded(
                 child: ListView.separated(
                   shrinkWrap: true,
-                  itemBuilder: (context, index){
+                  itemBuilder: (context, index) {
                     return ListTile(
-                      leading: Icon(Icons.account_circle, size: 60, color: Colors.black54,),
-                      title: Text(arrNames[index],style: TextStyle(fontSize: 22 ,fontStyle: FontStyle.italic ,fontWeight: FontWeight.w500, color: Colors.teal),),
-                      subtitle: Text('Status',textScaleFactor: 1.5),
+                      leading: Icon(
+                        Icons.account_circle,
+                        size: 60,
+                        color: Colors.black54,
+                      ),
+                      title: Text(
+                        arrNames[index],
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.teal),
+                      ),
+                      subtitle: Text('Status', textScaleFactor: 1.5),
                       trailing: Icon(Icons.double_arrow),
                     );
                   },
                   itemCount: arrNames.length,
-                  separatorBuilder: (context, index){
-                    return Divider(height: 20, thickness: 4,);
+                  separatorBuilder: (context, index) {
+                    return Divider(
+                      height: 20,
+                      thickness: 4,
+                    );
                   },
                 ),
               ),
@@ -86,14 +136,10 @@ class _groupsPageState extends State<groupsPage> {
     );
   }
 
-
-
-    // the logout function
+  // the logout function
   Future<void> logout(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
     Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => MyLogin()));
   }
-
-
 }
